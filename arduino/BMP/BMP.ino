286a4a336aa40d54c866b792c43f39c83802c870
qitas <qitas@qitas.cn>
2019-07-29 21:31:26 +0800

    add more sensor

--- arduino/BMP/BMP.ino
+++ arduino/BMP/BMP.ino
@@ -23,21 +23,6 @@
  * https://github.com/adafruit/Adafruit-BMP085-Library
  */
 
-/***************************************************
-  This is an example for the BMP085 Barometric Pressure & Temp Sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout
-  ----> https://www.adafruit.com/products/391
-  ----> http://www.adafruit.com/products/1603
-
-  These displays use I2C to communicate, 2 pins are required to interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
 
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
